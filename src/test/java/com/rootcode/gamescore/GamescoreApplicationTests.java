cfc304b2541d4540e11e4b03faefa16fb3dc2671
Malinda Pieris <malinda.p@aeturnum.com>
2024-12-03 14:10:26 +0530

    Initial commit

---
+++ src/test/java/com/rootcode/gamescore/GamescoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootcode.gamescore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamescoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
